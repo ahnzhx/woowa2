246e65bfa0c3522a77e1510fec60126d792e46c5
ahnzhx <modunaeggu@naver.com>
2019-08-03 14:38:31 +0900

    second woowa oop example project

---
+++ src/test/java/com/woowa/fooddomainservice/FoodDomainServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.woowa.fooddomainservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodDomainServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
